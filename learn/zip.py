49b2efa903b5da8eaf79bb55f60cc3f16a00d4d2
Naveen Kumar <31725874+naveenkumar-vr@users.noreply.github.com>
2018-06-23 13:36:22 +0530

    Update zip.py

--- learn/zip.py
+++ learn/zip.py
@@ -4,10 +4,7 @@ s_path = ""
 def zipd(ziph):
     for path,dirs,files in os.walk(s_path):
         for file in files:
-            l = [".xml",".txt",".docx",".py",".c",".pages",".pptx",".pdf",".doc",".zip"]
-            for i in l:
-                if file.endswith(i):
-                    ziph.write(os.path.join(path, file))
+            ziph.write(os.path.join(path, file))
 
 s_path = raw_input("Enter the directory: ")
 zipf = ZipFile(s_path + ".zip", 'w')
