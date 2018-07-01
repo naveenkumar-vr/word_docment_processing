8b57077774de6b84fc04506ea954f40886e43f5e
Naveen Kumar <31725874+naveenkumar-vr@users.noreply.github.com>
2018-07-01 16:43:37 +0530

    Update token.py

--- token.py
+++ token.py
@@ -8,24 +8,24 @@ TEXT = WORD_NAMESPACE + 't'
 word_punct_tokenizer = WordPunctTokenizer()
 
 def token_offsets(text):
-    l1= []
-    l = []
+    tokenoffset= []
+    token = []
     for i in range(len(text)):
         if text[i] == ' ':
-            l1.append(l)
-            l = []
+            tokenoffset.append(token)
+            token = []
         else:
-            l.append(i)
-    l1.append(l)
-    l1 = [x for x in l1 if x != []]
+            token.append(i)
+    tokenoffset.append(token)
+    tokenoffset = [x for x in tokenoffset if x != []]
     temp = []
-    for j in range(len(l1)):
-        if len(l1[j]) > 2:
-            temp.append(l1[j][0])
-            temp.append(l1[j][-1])
-            l1[j] = temp
+    for j in range(len(tokenoffset)):
+        if len(tokenoffset[j]) > 2:
+            temp.append(tokenoffset[j][0])
+            temp.append(tokenoffset[j][-1])
+            tokenoffset[j] = temp
             temp = []
-    return l1
+    return tokenoffset
 
 def check_and_embed_ids_in_paras(xml):
 #    """ Checks of unique ids are present in the paragraphs of the given main document, if not - embeds them"""
