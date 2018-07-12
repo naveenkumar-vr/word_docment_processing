69818bf06fbdd56d281fdf50a48cc28b078e004b
Naveen Kumar <31725874+naveenkumar-vr@users.noreply.github.com>
2018-07-12 20:41:49 +0530

    Update word_proces.py

--- word_proces.py
+++ word_proces.py
@@ -1,18 +1,9 @@
 import os
 from lxml import etree
 from zipfile import ZipFile
-# from variables_word import *
+from variables_word import *
 from collections import OrderedDict
 from nltk.tokenize import WordPunctTokenizer
-
-
-WORD_NAMESPACE = '{http://schemas.openxmlformats.org/wordprocessingml/2006/main}'
-PARA = WORD_NAMESPACE + 'p'
-TEXT = WORD_NAMESPACE + 't'
-BOLD = WORD_NAMESPACE + 'b'
-RUN = WORD_NAMESPACE + 'r'
-ITALIC = WORD_NAMESPACE + 'i'
-word_punct_tokenizer = WordPunctTokenizer()
 
 
 def unzip(path, file):
