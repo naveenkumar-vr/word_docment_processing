4c10127a3970afe37c300e20e430251cfc5dadf5
Naveen Kumar <31725874+naveenkumar-vr@users.noreply.github.com>
2018-07-19 22:01:48 +0530

    Update word_proces.py

--- word_proces.py
+++ word_proces.py
@@ -1,107 +1,125 @@
 import os
 from lxml import etree
+import zipfile
 from zipfile import ZipFile
 from variables_word import *
 from collections import OrderedDict
 from nltk.tokenize import WordPunctTokenizer
 
+class document:
 
-def unzip(path, file):
-    zip = ZipFile(path, 'r')
-    zip.extractall()
-    xml = ''
-    path_xml = path.replace(file, 'word')
-    for root, dirs, files in os.walk(path_xml):
-        if 'document.xml' in files:
-            xml = 'document.xml'
-    res = check_and_embed_ids_in_paras(xml)
-    return res
+    def __init__(self, path_file, file):
+        self.file = file
+        self.path_file = path_file
+        self.text_dict = OrderedDict()
+        self.token_offsets = []
+        self.tokens = []
+        self.xml = ''
 
 
-def token_offsets(text):
-    token = word_punct_tokenizer.tokenize(text)
-    count = 0
-    token_offset = []
-    token_of_tokenoffset = [0]
-    temp_text = ''
-    for i in range(len(text)):
-        if temp_text != token[0] and text[i] != ' ':
-            temp_text += text[i]
-            if token_of_tokenoffset[0] == 0:
-                token_of_tokenoffset[0] += count
-        if temp_text == token[0]:
-            token_of_tokenoffset.append(count)
-            token_offset.append(token_of_tokenoffset)
-            token.remove(temp_text)
-            temp_text = ''
-            token_of_tokenoffset = [0]
-        count += 1
-    return token_offset
+    def unzip(self):
+
+        zip = zipfile.ZipFile(self.path_file, 'r')
+        path_extracted_file = self.path_file.replace('/' + self.file, '')
+        zip.extractall(path_extracted_file)
+        path_xml = self.path_file.replace(self.file, 'word')
+        for root, dirs, files in os.walk(path_xml):
+            if 'document.xml' in files:
+                self.xml = 'document.xml'
 
 
-def check_and_embed_ids_in_paras(xml):
+    def token_offset(self, text):
 
-    text_dict = OrderedDict()
-    tree = etree.ElementTree(file=xml)
-    id_number = 0
-    token_offset = []
-    token = []
-    for para in tree.iter(PARA):
-        if para.attrib.get('id') is None:
-            para.attrib['id'] = str(id_number)
+        token = word_punct_tokenizer.tokenize(text)
+        count = 0
+        token_offset = []
+        token_of_tokenoffset = [0]
+        temp_text = ''
+        for i in range(len(text)):
+            if temp_text != token[0] and text[i] != ' ':
+                temp_text += text[i]
+                if token_of_tokenoffset[0] == 0:
+                    token_of_tokenoffset[0] += count
+            if temp_text == token[0]:
+                token_of_tokenoffset.append(count)
+                token_offset.append(token_of_tokenoffset)
+                token.remove(temp_text)
+                temp_text = ''
+                token_of_tokenoffset = [0]
+            count += 1
+        return token_offset
+
+
+    def check_and_embed_ids_in_paras(self):
+
+        tree = etree.ElementTree(file=self.xml)
+        id_number = 0
+        for para in tree.iter(PARA):
+            if para.attrib.get('id') is None:
+                para.attrib['id'] = str(id_number)
+                id_number += 1
+            texts = [node.text for node in para.iter(TEXT) if node.text]
+            if texts:
+                para_text = ''.join(texts).strip()
+                self.text_dict[para.attrib['id']] = {}
+                self.text_dict[para.attrib['id']]['text'] = para_text
+                copy_token_offset = self.token_offset(text=para_text)
+                self.token_offsets.append(copy_token_offset)
+                self.text_dict[para.attrib['id']]['token_offsets'] = copy_token_offset
+                copy_token = word_punct_tokenizer.tokenize(para_text)
+                self.tokens.append(copy_token)
+                self.text_dict[para.attrib['id']]['tokens'] = copy_token
+        return self.text_dict
+
+
+    def style(self, STYLE):
+
+        tree = etree.ElementTree(file=self.xml)
+        id_number = 0
+        id_number_list = []
+        for para in tree.iter(PARA):
+            style_text_list = []
+            style_tokenoffsets = []
+            texts = [node.text for node in para.iter(TEXT) if node.text]
+            if texts:
+                para_text = ''.join(texts).strip()
+                token_offsets = self.token_offset(text=para_text)
+                tokens = word_punct_tokenizer.tokenize(para_text)
+            for run in para.iter(RUN):
+                for style in run.iter(STYLE):
+                    style_parent = style.getparent()
+                    if style_parent.tag == run.tag + 'Pr':
+                        style_run_text_list = [node.text for node in run.iter(TEXT) if node.text]
+                        style_run_text = ''.join(style_run_text_list).strip()
+                        style_text_list.append(style_run_text)
+                style_text = ''.join(style_text_list).strip()
+                style_tokens = word_punct_tokenizer.tokenize(style_text)
+                index_style_tokenoffset = [j for i in range(len(style_tokens)) for j in range(len(tokens)) if style_tokens[i] == tokens[j]]
+                style_no_true_tokenoffset = [token_offsets[i] for i in index_style_tokenoffset]
+                for i in range(len(style_no_true_tokenoffset)):
+                    temp = []
+                    for offsets in style_no_true_tokenoffset[i]:
+                        temp.append(offsets)
+                    temp.append('true')
+                    if temp not in style_tokenoffsets: style_tokenoffsets.append(temp)
+            if texts:
+                if STYLE == BOLD: self.text_dict[str(id_number)]['bold'] = style_tokenoffsets
+                if STYLE == ITALIC: self.text_dict[str(id_number)]['italic'] = style_tokenoffsets
             id_number += 1
-        texts = [node.text for node in para.iter(TEXT) if node.text]
-        if texts:
-            para_text = ''.join(texts).strip()
-            text_dict[para.attrib['id']] = {}
-            text_dict[para.attrib['id']]['text'] = para_text
-            temp_token_offset = token_offsets(para_text)
-            token_offset.append(temp_token_offset)
-            text_dict[para.attrib['id']]['token_offsets'] = temp_token_offset
-            temp_token = word_punct_tokenizer.tokenize(para_text)
-            token.append(temp_token)
-            text_dict[para.attrib['id']]['tokens'] = temp_token
-        bold_list = []
-        bold_true = []
-        italic_list = []
-        italic_true = []
-        for run in para.iter(RUN):
-            for bold in run.iter(BOLD):
-                bold_parent = bold.getparent()
-                if bold_parent.tag == run.tag + 'Pr':
-                        bold_text = [node.text for node in run.iter(TEXT) if node.text]
-                        bold_texts = ''.join(bold_text).strip()
-                        bold_list.append(bold_texts)
-            for italic in run.iter(ITALIC):
-                italic_parent = italic.getparent()
-                if italic_parent.tag == run.tag + 'Pr':
-                        italic_text = [node.text for node in run.iter(TEXT) if node.text]
-                        italic_texts = ''.join(italic_text).strip()
-                        italic_list.append(italic_texts)
-        bold_lists = ''.join(bold_list).strip()
-        italic_lists = ''.join(italic_list).strip()
-        bold_tokens = word_punct_tokenizer.tokenize(bold_lists)
-        italic_tokens = word_punct_tokenizer.tokenize(italic_lists)
-        index_bold_tokenoffset = [j for i in range(len(bold_tokens)) for j in range(len(temp_token)) if bold_tokens[i] == temp_token[j]]
-        index_italic_tokenoffset = [j for i in range(len(italic_tokens)) for j in range(len(temp_token)) if italic_tokens[i] == temp_token[j]]
-        bold_temp_list = [temp_token_offset[i] for i in index_bold_tokenoffset]
-        italic_temp_list = [temp_token_offset[i] for i in index_italic_tokenoffset]
-        for i in bold_temp_list:
-            if 'true' not in i:
-                i.append('true')
-                bold_true.append(i)
-        for i in italic_temp_list:
-            if 'true' not in i:
-                i.append('true')
-                italic_true.append(i)
-        if texts:
-            text_dict[para.attrib['id']]['bold'] = bold_true
-            text_dict[para.attrib['id']]['italic'] = italic_true
-    tree.write(xml, encoding='UTF-8', standalone=True, pretty_print=True)
-
-    return text_dict
+        return self.text_dict
 
 
-file = raw_input("Enter the file name: ")
-path = raw_input("Enter the path to file: ")
-print unzip(path, file)
+
+    def para_dict(self):
+        self.unzip()
+        check_and_embed_ids_in_paras = self.check_and_embed_ids_in_paras()
+        bold_style = self.style(BOLD)
+        style = self.style(ITALIC)
+        return style
+
+
+
+files = raw_input("Enter the file name: ")
+path_files = raw_input("Enter the path to file: ")
+text_dict = document(path_file=path_files, file=files)
+print text_dict.para_dict()
